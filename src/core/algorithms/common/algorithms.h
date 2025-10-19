ece98b5a6e79ad0e4f7a2e015676f8aa1fd7107e
Artyom Gavrilov <artyom.e.gavrilov@gmail.com>
2025-10-19 17:50:51 +0300

    works
    # Conflicts:
    #	src/core/algorithms/gfd/gfd_validator/gfd_handler.h
    #	src/core/algorithms/gfd/gfd_validator/gfd_validator.h
    #	src/core/algorithms/gfd/gfd_validator/naivegfd_validator.h
    #	src/core/algorithms/ind/ind_algorithm.h
    #	src/core/algorithms/ind/ind_verifier/ind_verifier.h
    #	src/core/algorithms/ind/mind/mind.cpp
    #	src/core/algorithms/md/md_algorithm.h
    #	src/core/algorithms/md/md_verifier/md_verifier.h
    #	src/core/algorithms/metric/metric_verifier.h
    #	src/core/algorithms/nar/nar_algorithm.h
    #	src/core/algorithms/nd/nd_verifier/nd_verifier.h
    #	src/core/algorithms/od/fastod/fastod.h
    #	src/core/algorithms/od/order/order.h
    #	src/core/algorithms/ucc/ucc_algorithm.h
    #	src/core/algorithms/ucc/ucc_verifier/ucc_verifier.h

---
+++ src/core/algorithms/common/algorithms.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "algebraic_constraints/mining_algorithms.h"
+
+#include "association_rules/mining_algorithms.h"
+
+#include "cfd/cfd_verifier/cfd_verifier.h"
+#include "cfd/mining_algorithms.h"
+
+#include "dd/mining_algorithms.h"
+
+#include "fd/mining_algorithms.h"
+#include "fd/sfd/cords.h"
+#include "fd/verification_algorithms.h"
+
+#include "gfd/gfd_validator/verification_algorithms.h"
+
+#include "ind/ind_verifier/ind_verifier.h"
+#include "ind/mining_algorithms.h"
+
+#include "md/mining_algorithms.h"
+#include "md/verification_algorithms.h"
+
+#include "metric/verification_algorithms.h"
+
+#include "nar/mining_algorithms.h"
+
+#include "od/mining_algorithms.h"
+
+#include "statistics/algorithms.h"
+
+#include "ucc/mining_algorithms.h"
+#include "ucc/verification_algorithms.h"
