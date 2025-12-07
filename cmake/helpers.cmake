c42b40465fd17b818e6dbf7b383e46b6e6bc2042
Artyom Gavrilov <artyom.e.gavrilov@gmail.com>
2025-12-08 01:38:27 +0300

    rename

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -1,62 +1,61 @@
-function(add_headers TARGET SCOPE)
+function(add_headers target scope)
     set(files ${ARGN})
 
-    if(NOT TARGET ${HD_TARGET})
-        message(FATAL_ERROR "[add_headers] TARGET \"${HD_TARGET}\" не существует.")
+    if(NOT TARGET ${target})
+        message(FATAL_ERROR "[add_headers] Target wasn't found: \"${target}\"")
     endif()
 
-    if(SCOPE STREQUAL "PUBLIC")
-        set(FILESET_NAME "HEADERS")
-    elseif(SCOPE STREQUAL "PRIVATE")
-        set(FILESET_NAME "privateHeaders")
-    elseif(SCOPE STREQUAL "INTERFACE")
-        set(FILESET_NAME "interfaceHeaders")
+    if(scope STREQUAL "PUBLIC")
+        set(file_set_name "HEADERS")
+    elseif(scope STREQUAL "PRIVATE")
+        set(file_set_name "privateHeaders")
+    elseif(scope STREQUAL "INTERFACE")
+        set(file_set_name "interfaceHeaders")
     else()
         message(FATAL_ERROR
-                "[add_headers] Некорректная SCOPE: \"${HD_SCOPE}\". "
-                "Ожидается PUBLIC, PRIVATE или INTERFACE."
+                "[add_headers] Invalid scope for target \"${target}\": \"${scope}\"."
+                "Possible scopes: PRIVATE, PUBLIC, INTERFACE."
         )
     endif()
 
-    target_sources(${TARGET}
-        ${SCOPE}
-            FILE_SET ${FILESET_NAME}
+    target_sources(${target}
+        ${scope}
+            FILE_SET ${file_set_name}
             TYPE HEADERS
-            BASE_DIRS ${PROJECT_SOURCE_DIR}/src
+            BASE_DIRS ${PROJECT_SOURCE_DIR}/src #todo: make good
             FILES ${files}
     )
 endfunction()
 
-function(target_link_with_given GIVEN SCOPE)
-    foreach(TARGET IN LISTS GIVEN)
-        if(NOT TARGET ${TARGET})
-            message(FATAL_ERROR "[target_link_with_given] TARGET '${TARGET}' doesn't exist.")
+function(target_link_with_given given scope)
+    foreach(target IN LISTS given)
+        if(NOT TARGET ${target})
+            message(FATAL_ERROR "[target_link_with_given] Target wasn't found: \"${target}\"")
         endif()
     endforeach()
 
-    foreach(TARGET IN LISTS ARGN)
-        if(TARGET ${TARGET})
-            target_link_libraries(${TARGET} ${SCOPE} ${GIVEN})
-        else()
-            message(FATAL_ERROR "Target '${TARGET}' doesn't exist")
+    foreach(target IN LISTS ARGN)
+        if(NOT TARGET ${target})
+            message(FATAL_ERROR "[target_link_with_given] Target wasn't found: \"${target}\"")
         endif()
+        target_link_libraries(${target} ${scope} ${given})
     endforeach()
 endfunction()
 
-function(Desbordante_AddTarget NAME)
+function(Desbordante_AddTarget name)
     set(singleVals TYPE)
     set(multiVals _SRCS _LIBS)
 
     cmake_parse_arguments(args "" "${singleVals}" "${multiVals}" ${ARGN})
     if(args_TYPE STREQUAL "EXE")
-        add_executable(${NAME} ${args_SRCS})
+        add_executable(${name} ${args_SRCS})
     elseif(args_TYPE STREQUAL "LIB")
-        add_library(${NAME} ${args_SRCS}) #todo: target_srcs?
+        add_library(${name} ${args_SRCS}) #todo: target_srcs?
     else()
         message(FATAL_ERROR "add_target: TYPE must be EXE or LIB")
     endif()
-    set_target_properties(${NAME} PROPERTIES LINK_LIBRARIES_ONLY_TARGETS ON)
+    set_target_properties(${name} PROPERTIES LINK_LIBRARIES_ONLY_TARGETS ON)
     if(args_LIBS)
-        target_link_libraries(${NAME} PRIVATE ${args_LIBS})
+        target_link_libraries(${name} PRIVATE ${args_LIBS})
     endif()
 endfunction()
