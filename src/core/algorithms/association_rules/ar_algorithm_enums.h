0e67e755e336628ddfa45be084017e23be291662
Artyom Gavrilov <artyom.e.gavrilov@gmail.com>
2026-03-01 16:02:15 +0300

    Add visibility attribute for enum

--- src/core/algorithms/association_rules/ar_algorithm_enums.h
+++ src/core/algorithms/association_rules/ar_algorithm_enums.h
@@ -3,7 +3,7 @@
 #include <enum.h>
 
 namespace algos {
-
+#include "core/model/enum_visibility_begin.h"
 BETTER_ENUM(InputFormat, char, singular = 0, tabular);
-
+#include "core/model/enum_visibility_end.h"
 }  // namespace algos
